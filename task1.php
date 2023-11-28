808cc909ba10ccbc542cd29006b68fe6288ee197
v <hamzehalmajali@icloud.com>
2023-11-28 15:14:49 +0300

    php tasks

---
+++ task1.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php // task1
+    // echo "<h1>Client's IP Address: " . $ipAddress ."</h1>";
+    // echo phpinfo();
+    // task2
+    // echo "Towmorro i'll learn php global variables";
+    // echo "Towmorro i'll learn php global variables";
+    // echo "this is a bad command c:\.";
+
+//  <!-- task 3 -->
+//     <!-- <form action="" method="GET">
+//     <label for="name">name</label>
+//     <input type="text" name="name">
+
+//     </form>
+   
+    // if(isset($_GET['name'])){
+    // echo $_GET['name'];
+    // }
+    // task 4
+  // if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    //     $ipAddress = $_SERVER['HTTP_CLIENT_IP'];
+    // } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    //     $ipAddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    // } else {
+    //     $ipAddress = $_SERVER['REMOTE_ADDR'];
+    // }
+    
+    // task 5
+        // $filename=$_SERVER['PHP_SELF'];
+        // echo'YOUR CURRNENT FILE IS'.$filename.'';
+        
+        
+        
+        // task 6
+    // $url = "https://www.w3schools.com/php/default.asp";
+
+    
+    // $urlComponents = parse_url($url);
+    // echo "Scheme: " . $urlComponents['scheme'] . "<br>";
+    // echo "Host: " . $urlComponents['host'] . "<br>";
+    // echo "Path: " . $urlComponents['path'] . "<br>";
+    // echo "Query: " . (isset($urlComponents['query']) ? $urlComponents['query'] : 'N/A') . "<br>";
+    // echo "Fragment: " . (isset($urlComponents['fragment']) ? $urlComponents['fragment'] : 'N/A') . "<br>";
+      // task 7
+   
+// $string = "PHP Tutorial";
+// $words = explode(' ', $string);
+// foreach ($words as &$word) {
+//       $word = "<span style='color: red;'>" . substr($word, 0, 1) . "</span>" . substr($word, 1);
+// }
+// $modifiedString = implode(' ', $words);
+// echo $modifiedString;
+
+
+  // task 8
+    // header("Location: https://www.w3schools.com/");
+// exit();
+
+    
+  
+    
+    
+    
+    ?>  
+   
+    
+
+-->
+
+</body>
+</html>
